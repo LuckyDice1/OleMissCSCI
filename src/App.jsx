c61e2eae824cce4de1295f8123d0208744cfd7b2
earnest35 <119082338+earnest35@users.noreply.github.com>
2023-06-04 07:41:27 -0500

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar, Nav, Button, Dropdown, Form, Collapse } from 'bootstrap-4-react';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="topNavbar">
+      <Navbar expand="lg" light bg="light" >
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Toggler target="#navbarNav" />
+        <Collapse navbar id="navbarNav">
+          <Navbar.Nav>
+            <Nav.ItemLink href="#" active>Home</Nav.ItemLink>
+            <Nav.ItemLink href="#">Projects</Nav.ItemLink>
+            <Nav.ItemLink href="#">Contact</Nav.ItemLink>
+            <Nav.Item dropdown>
+              <Nav.Link dropdownToggle>Resources</Nav.Link>
+              <Dropdown.Menu>
+                <Dropdown.Item>Action</Dropdown.Item>
+                <Dropdown.Item>Another action</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Nav.Item>
+          </Navbar.Nav>
+        </Collapse>
+      </Navbar>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-       CHICKWINgGGGGGGGGGGGGGGGGGGGGGGGGGGSldkjslkjgjgksklgkghg
-      </p>
-    </>
   )
 }
 
